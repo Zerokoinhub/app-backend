c611175f7e21474a1e1b473879dc3ee4c02968f7
Safi32 <shaheershahid75@gmail.com>
2025-06-15 20:52:18 +0500

    Fixed Crashing issue

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -18,16 +18,16 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { verifyFirebaseToken } = require('../middleware/firebaseAuth');
-const { getUserSessions, unlockNextSession } = require('../controllers/userController');
 
+// Public routes
 router.post('/register', userController.registerUser);
 router.get('/invite/:inviteCode', userController.getInviteDetails);
 router.post('/referral', userController.processReferral);
-router.get('/sessions', verifyFirebaseToken, getUserSessions);
-router.post('/unlock', verifyFirebaseToken, unlockNextSession);
+router.get('/count', userController.getUserCount);
+
+// Protected routes (require Firebase authentication)
 router.post('/sync', verifyFirebaseToken, userController.syncFirebaseUser);
 router.get('/profile', verifyFirebaseToken, userController.getUserProfile);
 router.put('/wallet-address', verifyFirebaseToken, userController.updateWalletAddress);
-router.get('/count', userController.getUserCount);
 
 module.exports = router;
