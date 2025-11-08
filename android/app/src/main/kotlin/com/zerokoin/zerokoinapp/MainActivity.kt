3ce3d51a1790c33132c89c90948657d9ef970f40
shoaib3474 <shoaib3974@gmail.com>
2025-11-08 20:54:38 +0500

    delete frontend code from here

--- android/app/src/main/kotlin/com/zerokoin/zerokoinapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.zerokoin.zerokoinapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/constant/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color blue = Color(0xFF067A98);
-  static const Color lightBlue = Color(0xFF2B9ED4);
-}
