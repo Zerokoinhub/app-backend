21a3062182a2a52c68d10ba763a58235b86b5590
farooqahmed630 <fm8971421@gmail.com>
2026-04-16 23:28:34 +0500

    Add route to get courses by language

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -5,5 +5,5 @@ const courseController = require('../controllers/courseController');
 // Public routes - Get all courses
 router.get('/all', courseController.getAllCourses);
 router.get('/:courseName', courseController.getCourseDetailsByName); // New route to get course details by name
-
+router.get('/language/:language', courseController.getCoursesByLanguage);
 module.exports = router;
