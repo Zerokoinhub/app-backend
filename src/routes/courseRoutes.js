1f2c3f62526e2070f02ccaee1afe793caa84c6f9
farooqahmed630 <fm8971421@gmail.com>
2026-05-08 16:42:06 +0500

    Update courseRoutes.js

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
 
+// ✅ ADD THIS - Create new course (POST)
+router.post('/', courseController.addCourse);
+
 // Public routes - Get all courses
 router.get('/all', courseController.getAllCourses);
-router.get('/:courseName', courseController.getCourseDetailsByName); // New route to get course details by name
+router.get('/:courseName', courseController.getCourseDetailsByName);
 router.get('/language/:language', courseController.getCoursesByLanguage);
+
 module.exports = router;
